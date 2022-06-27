11172681110197001f173c280c82d33c3fc6ceaa
thenithinbalaji <thenithinbalaji@gmail.com>
2022-06-27 17:46:02 +0530

    removed qn repetitions
    added cachelist to check if questions were answered already

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+const cachelist = []
+var previndex = -1
+
 function next(){
+    //put random qn in question h1 element
     randomqn()
-    var audio = new Audio('assets/press.mp3');
+
+    //play press audio
+    const audio = new Audio('assets/press.mp3');
     audio.play();
 
+    //restart animations
     question = document.getElementById("question")
-
     question.style.animation = 'none';
     question.offsetHeight;
     question.style.animation = null; 
@@ -12,12 +18,22 @@ function next(){
 }
 
 async function randomqn(){
+  //change this in production
+  // const response = await fetch("https://raw.githubusercontent.com/thenithinbalaji/YES-or-NO/main/assets/questions.json")
+  const response = await fetch("http://192.168.0.111:5500//assets/questions.json")
 
-  question = document.getElementById("question")
-  
-  const response = await fetch("https://raw.githubusercontent.com/thenithinbalaji/YES-or-NO/main/assets/questions.json")
   const data = await response.json()
 
-  question.innerHTML = data[Math.floor(Math.random() * (Object.keys(data).length) - 1)];
+  qnno = Math.floor(Math.random() * (Object.keys(data).length))
+  while(cachelist.includes(qnno)){
+    qnno = Math.floor(Math.random() * (Object.keys(data).length))
+  }
+
+  document.getElementById("question").innerHTML = data[qnno];
+  
+  cachelist.push(qnno);
+  previndex = qnno;
+
+
 }
 
